1b404ebf1245f1478dfc564f28af326452a7cb62
GeoffreyPaul3 <110961738+Gtigercode@users.noreply.github.com>
2024-02-13 22:51:57 +0200

    fixed events errors

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,5 +1,5 @@
 import EventForm from "@/components/shared/EventForm"
-import { getEventById } from "@/lib/actions/event.actions"
+
 import { auth } from "@clerk/nextjs";
 
 type UpdateEventProps = {
@@ -12,8 +12,7 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
-  const event = await getEventById(id)
-
+  
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -22,9 +21,7 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
 
       <div className="wrapper my-8">
         <EventForm 
-          type="Update" 
-          event={event} 
-          eventId={event._id} 
+          type="Update"  
           userId={userId} 
         />
       </div>
